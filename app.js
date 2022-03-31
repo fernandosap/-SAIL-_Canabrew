35c2235c7f4a214ca9a6775f37a67ec523cb7b8b
Fernando Sanchez <fernando.sanchez@sap.com>
2022-03-31 21:47:46 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+const path = require("path");
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var port = 8080;
+
+app.get('/',function(req,res){
+	res.render("index")
+});
+
+app.get('/index.html',function(req,res){
+	res.redirect('/');
+});
+
+app.get('/about',function(req,res){
+	res.render("about")
+});
+
+app.get('/contact',function(req,res){
+	res.render("contact")
+});
+
+app.get('/srp',function(req,res){
+	res.render("srp")
+});
+
+app.get('/more',function(req,res){
+	res.render("more")
+});
+
+
+app.listen(port,function(req,res){
+	console.log('Escuchando en el puerto: ' + port);
+});
+
